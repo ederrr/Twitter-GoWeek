f39ad631a4225b25cc9a5ff23995ec13feb4817d
Eder RR <ederrosate@yahoo.com.br>
2019-02-04 21:10:00 -0200

    frontend finished

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import Tweet from '../components/Tweet'
 import twitterLogo from '../twitter.svg';
+import socket from 'socket.io-client'
+
 import './Timeline.css';
 
 class Timeline extends Component {
 
 	state ={
+		tweets: [],
 		newTweet: '',
 	};
+	async componentDidMount(){
+		this.subscribeToEvents();
+		const response = await api.get('tweets');
+		this.setState({tweets: response.data});
+	}
+
+	subscribeToEvents = () => {
+		const io = socket('http://localhost:3000');
+		io.on('tweet', data =>{
+			this.setState({ tweets: [data, ...this.state.tweets]})
+		});
+		io.on('like', data =>{
+			this.setState({ tweets: this.state.tweets.map(
+				tweet =>(tweet._id === data._id ? data : tweet)
+			)})
+		})
+	}
 
 	handleInputChange = (e) =>{
 		this.setState({ newTweet: e.target.value});
@@ -31,6 +52,9 @@ class Timeline extends Component {
 				<form>
 					<textarea value={this.state.newTweet} onChange={this.handleInputChange} onKeyDown={this.handleNewTweet} placeholder="O que está acontecendo?" ></textarea>
 				</form>
+				<ul className="tweet-list">
+					{this.state.tweets.map(tweet => <Tweet key={tweet._id} tweet={tweet} />)}
+				</ul>
 			</div>
 		);
 	}
